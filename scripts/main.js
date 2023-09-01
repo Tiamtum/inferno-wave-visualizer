40a308f610d6e19ab1f7edaa83821daee52b1642
Morgan Chafe <mc.github@outlook.com>
2023-09-01 16:14:20 -0230

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+const previousWaveBtn = document.querySelector("#previousWaveBtn");
+const nextWaveBtn = document.querySelector("#nextWaveBtn");
+const imageContainer = document.querySelector("imageContainer")
+
+// fetch("scripts/waves.json")
+//     .then((response)=> response.json())
+//     .then((json) => console.log(json));
